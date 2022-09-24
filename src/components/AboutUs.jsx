f04bf3b0a96c454703004f7a2c4fcf39b107e9cf
gift56 <efeasiughu@gmail.com>
2022-09-24 23:36:46 +0100

    done with jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Label from "../assets/Alabel.svg";
 import Image1 from "../assets/aboutMI.svg";
 import Image2 from "../assets/desktopAImg.svg";
+import sideImg from "../assets/sideImg.svg";
+import BgLine from "../assets/Bglines.svg";
 import RamP from "../assets/RAMP.svg";
 
 import {
@@ -17,14 +19,45 @@ const AboutUs = () => {
   return (
     <AboutContainer>
       <Container>
-        <AboutArea>
+        <AboutArea bg={BgLine}>
           <AboutLeft>
             <AboutTitleArea>
               <h2>About US</h2>
             </AboutTitleArea>
           </AboutLeft>
           <AboutRight>
-            <div className="Image"></div>
+            <div className="mobileImage">
+              <img src={Image1} alt="/" />
+            </div>
+            <div className="desktopImage">
+              <img src={Image2} alt="/" />
+            </div>
+            <div className="text">
+              <h3>High quality</h3>
+              <p className="mobileP">
+                <span>The hotel was reopened to visitors in 2016. The</span>{" "}
+                <span>building was renovated and modernized to meet</span>{" "}
+                <span>the expectations of the most demanding guests.</span>{" "}
+                <span>We offer luxurious rooms, numerous facilities, and</span>{" "}
+                <span>exceptional service. The hotel is located near the</span>{" "}
+                <span>city center, which makes it the best option for</span>
+                <span>those who travel for business and travel purposes.</span>
+              </p>
+              <p className="deskP">
+                The hotel was reopened to visitors in 2016. The building was
+                renovated and modernized to meet the expectations of the most
+                demanding guests. We offer luxurious rooms, numerous facilities,
+                and exceptional service.
+              </p>
+              <img src={Label} alt="/" />
+            </div>
+            <div className="tab">
+              <div className="tabsideImg">
+                <img src={sideImg} alt="/" />
+              </div>
+              <img src={Label} alt="/" className="labelImg" />
+              <img src={RamP} alt="rooms" />
+            </div>
           </AboutRight>
         </AboutArea>
       </Container>
--- src/styles/about.styled.js
+++ src/styles/about.styled.js
@@ -1,11 +1,70 @@
 import styled from "styled-components";
 
-export const AboutContainer = styled.section``;
+export const AboutContainer = styled.section`
+  position: relative;
+  background-color: ${({ theme }) => theme.darkColor};
+`;
 
-export const AboutArea = styled.div``;
+export const AboutArea = styled.div`
+  padding: 30px 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  justify-content: flex-start;
+  align-items: flex-start;
+  @media screen and (min-width: ${({ theme }) => theme.mobile.tab}) {
+    padding-top: 20px;
+    padding-bottom: 50px;
+  }
+`;
 
 export const AboutLeft = styled.div``;
 
-export const AboutRight = styled.div``;
+export const AboutRight = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .mobileImage {
+    width: 294px;
+    img {
+      width: 100%;
+    }
+    @media screen and (min-width: 983px) {
+      display: none;
+    }
+    @media screen and (min-width: 583px) {
+      width: 90%;
+    }
+  }
+  .desktopImage {
+    display: none;
+    width: 100%;
+    @media screen and (min-width: 983px) {
+      display: block;
+    }
+  }
+  .text {
+    .mobileP {
+      @media screen and (min-width: ${({ theme }) => theme.mobile.tab}) {
+        display: none;
+      }
+    }
+    .deskP {
+      display: none;
+      @media screen and (min-width: ${({ theme }) => theme.mobile.tab}) {
+        display: block;
+      }
+    }
+  }
+  .tab {
+    display: none;
+    @media screen and (min-width: 983px) {
+      display: block;
+    }
+  }
+`;
 
 export const AboutTitleArea = styled.div``;
