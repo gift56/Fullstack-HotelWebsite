0d6552a33de3d32cc57031864cb74cce80caff9b
gift56 <efeasiughu@gmail.com>
2022-09-24 21:18:57 +0100

    added some stuff

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { AboutContainer } from '../styles/about.styled';
-import { Container } from '../styles/container.styled';
+import React from "react";
+import Label from "../assets/Alabel.svg";
+import Image1 from "../assets/aboutMI.svg";
+import {
+  AboutArea,
+  AboutContainer,
+  AboutLeft,
+  AboutRight,
+  AboutTitleArea,
+} from "../styles/about.styled";
+import { Container } from "../styles/container.styled";
 
 const AboutUs = () => {
   return (
     <AboutContainer>
-        <Container>
-            
-        </Container>
+      <Container>
+        <AboutArea>
+          <AboutLeft>
+            <AboutTitleArea>
+              <h2>About US</h2>
+            </AboutTitleArea>
+          </AboutLeft>
+          <AboutRight>
+            <div className="Image"></div>
+          </AboutRight>
+        </AboutArea>
+      </Container>
     </AboutContainer>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- src/styles/about.styled.js
+++ src/styles/about.styled.js
@@ -5,3 +5,7 @@ export const AboutContainer = styled.section``;
 export const AboutArea = styled.div``;
 
 export const AboutLeft = styled.div``;
+
+export const AboutRight = styled.div``;
+
+export const AboutTitleArea = styled.div``;
