5a91d78d04f953d5b6a13ff174e6988313d42122
gift56 <efeasiughu@gmail.com>
2022-09-26 12:54:13 +0100

    started styling

--- src/styles/roomandapartment.styled.js
+++ src/styles/roomandapartment.styled.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
-export const RoomContainer = styled.section``;
+export const RoomContainer = styled.section`
+  position: relative;
+  background-color: ${({ theme }) => theme.whiteColor};
+`;
 
-export const RoomArea = styled.div``;
+export const RoomArea = styled.div`
+  padding: 40px 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
 
 export const RoomLeft = styled.div``;
 
