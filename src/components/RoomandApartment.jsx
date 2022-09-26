c87faee9e09f51b417ccbf913849baf9eddc984a
gift56 <efeasiughu@gmail.com>
2022-09-26 11:44:35 +0100

    Imported required stuff

--- src/components/RoomandApartment.jsx
+++ src/components/RoomandApartment.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import RightImge from "../assets/rmMain.svg";
+import MiniRightImg from "../assets/dRM.svg";
+import NextArrow from "../assets/nextBtn.svg";
+import Button from "./Button";
+import { Container } from "../styles/container.styled";
+import {
+  RoomArea,
+  RoomContainer,
+  RoomLeft,
+  RoomRight,
+} from "../styles/roomandapartment.styled";
 
 const RoomandApartment = () => {
   return (
-    <div>RoomandApartment</div>
-  )
-}
+    <RoomContainer>
+      <Container>
+        <RoomArea>
+          <RoomLeft></RoomLeft>
+          <RoomRight></RoomRight>
+        </RoomArea>
+      </Container>
+    </RoomContainer>
+  );
+};
 
-export default RoomandApartment
+export default RoomandApartment;
--- src/styles/roomandapartment.styled.js
+++ src/styles/roomandapartment.styled.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const RoomContainer = styled.section``;
 
+export const RoomArea = styled.div``;
+
 export const RoomLeft = styled.div``;
 
 export const RoomRight = styled.div``;
