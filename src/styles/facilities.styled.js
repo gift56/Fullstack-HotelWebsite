b66e7394d693c5a2b05617ce09dc07edc22b579f
gift56 <efeasiughu@gmail.com>
2022-10-06 11:43:10 +0100

    added facilities styles

---
+++ src/styles/facilities.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FacilitiesCon = styled.section``;
+
+export const FacilitiesArea = styled.div``;
+
+export const FacilitiesLeftside = styled.div``;
+
+export const FacilitiesLeftsideTitle = styled.div``;
+
+export const FacilitiesRightside = styled.div``;
