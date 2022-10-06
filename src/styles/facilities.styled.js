1d3cc31f23d0c99308ffe786ddc64c478fbc5885
gift56 <efeasiughu@gmail.com>
2022-10-06 13:27:50 +0100

    stying leftside

--- src/styles/facilities.styled.js
+++ src/styles/facilities.styled.js
@@ -2,9 +2,17 @@ import styled from "styled-components";
 
 export const FacilitiesCon = styled.section`
   border-top: 1px solid #313f388f;
+  position: relative;
 `;
 
-export const FacilitiesArea = styled.div``;
+export const FacilitiesArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 40px 0;
+`;
 
 export const FacilitiesLeftside = styled.div``;
 
