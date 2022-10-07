ec98e5d8c2f5b32d99b3b869889f9d6f60b1164b
gift56 <efeasiughu@gmail.com>
2022-10-07 13:14:07 +0100

    imported required img

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import MobileGalleyOne from "../assets/Mgallery1.svg";
+import MobileGalleyTwo from "../assets/Mgallery2.svg";
+import MobileGalleyThree from "../assets/Mgallery3.svg";
+import DesktopGalleyOne from "../assets/Dgallery1.svg";
+import DesktopGalleyTwo from "../assets/Dgallery2.svg";
+import DesktopGalleyThree from "../assets/Dgallery3.svg";
+import DesktopGalleyFour from "../assets/Dgallery4.svg";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  return <div>Gallery</div>;
+};
 
-export default Gallery
+export default Gallery;
