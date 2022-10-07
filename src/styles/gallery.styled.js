f89879db7b76193cb2d548023ab99b591dc6d148
gift56 <efeasiughu@gmail.com>
2022-10-07 13:05:37 +0100

    working on styled area

---
+++ src/styles/gallery.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const GalleryCon = styled.section``;
+
+export const GalleryArea = styled.div``;
+
+export const GalleryleftSide = styled.div``;
+
+export const GalleryrightSide = styled.div``;
+
+export const GalleryRightSideMobile = styled.div``;
+
+export const GalleryRightSideDesktop = styled.div``;
