0e62df47565d47a50212eff1e0bf12ffd6c28fab
gift56 <efeasiughu@gmail.com>
2022-10-07 11:54:00 +0100

    working on some stuff

--- src/styles/bestApartment.styled.js
+++ src/styles/bestApartment.styled.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const BestApartmentCon = styled.section`
   background-image: url(${({ bg }) => bg[1]});
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
 `;
 
 export const BestApartmentArea = styled.div``;
