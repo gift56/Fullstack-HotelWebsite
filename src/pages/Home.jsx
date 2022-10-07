aca725c3575794b165e65cbec8852c05bd256a80
gift56 <efeasiughu@gmail.com>
2022-10-07 13:01:18 +0100

    working on gallery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AboutUs from "../components/AboutUs";
 import BestApartment from "../components/BestApartment";
 import Facilities from "../components/Facilities";
+import Gallery from "../components/Gallery";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import RoomandApartment from "../components/RoomandApartment";
@@ -15,6 +16,7 @@ const Home = () => {
       <RoomandApartment />
       <Facilities />
       <BestApartment />
+      <Gallery />
     </>
   );
 };
