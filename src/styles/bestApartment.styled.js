126cf0c87358fed91e8c3e4a5302c4459b01ffab
gift56 <efeasiughu@gmail.com>
2022-10-07 12:05:10 +0100

    working on styling h2

--- src/styles/bestApartment.styled.js
+++ src/styles/bestApartment.styled.js
@@ -12,12 +12,21 @@ export const BestApartmentCon = styled.section`
 
 export const BestApartmentArea = styled.div`
   padding: 40px 0;
+  width: 100%;
   height: 98vh;
 `;
 
 export const BestApartmentTexts = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   height: 100%;
   justify-content: space-between;
+  h2 {
+    color: ${({ theme }) => theme.spanText};
+    font-weight: 400;
+    font-size: 50px;
+    line-height: 60px;
+    text-transform: uppercase;
+  }
 `;
