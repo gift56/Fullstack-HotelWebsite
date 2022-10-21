16ef13794eb88d08394f7564af4075b471067e2b
gift56 <efeasiughu@gmail.com>
2022-10-21 13:56:57 +0100

    sovled 90%

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AboutUs from "../components/Home/AboutUs";
+import BestApartment from "../components/Home/BestApartment";
+import Facilities from "../components/Home/Facilities";
+import Gallery from "../components/Home/Gallery";
+import GetInTourch from "../components/Home/GetInTourch";
+import Hero from "../components/Home/Hero";
+import Layout from "../components/Layout/Layout";
+import RoomandApartment from "../components/RoomandApartment";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Hero />
+      <AboutUs />
+      <RoomandApartment />
+      <Facilities />
+      <BestApartment />
+      <Gallery />
+      <GetInTourch />
+    </Layout>
+  );
+};
+
+export default Home;
