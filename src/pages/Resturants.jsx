d3c5e86400578b14c4349240d44dc2624d0459f9
gift56 <efeasiughu@gmail.com>
2022-10-21 13:43:29 +0100

    created two new pages

--- src/pages/Resturants.jsx
+++ src/pages/Resturants.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const About = () => {
+const Resturants = () => {
   return <Layout></Layout>;
 };
 
-export default About;
+export default Resturants;
--- src/routes/RoutedPages.jsx
+++ src/routes/RoutedPages.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import About from "../pages/About";
 import Home from "../pages/Home";
+import Resturants from "../pages/Resturants";
 import Room from "../pages/Room";
 
 const RoutedPages = () => {
@@ -9,6 +11,8 @@ const RoutedPages = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rooms" element={<Room />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/restaurant" element={<Resturants />} />
       </Routes>
     </>
   );
