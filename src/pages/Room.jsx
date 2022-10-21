7668762159a34252db3a33a05c2e6eb8b70f9cb1
gift56 <efeasiughu@gmail.com>
2022-10-21 15:57:18 +0100

    good night

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import RoomDetail from "../components/RoomsData/RoomDetail";
 import RoomHero from "../components/RoomsData/RoomHero";
 
 const Room = () => {
   return (
     <Layout>
       <RoomHero />
+      <RoomDetail />
     </Layout>
   );
 };
