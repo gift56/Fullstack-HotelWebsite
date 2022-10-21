3f0613e937c050b3a5cfee6d8bbd52c571707014
gift56 <efeasiughu@gmail.com>
2022-10-21 14:41:40 +0100

    working on rome homepage

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import RoomHero from "../components/RoomsData/RoomHero";
 
 const Room = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <RoomHero />
+    </Layout>
+  );
 };
 
 export default Room;
---
+++ src/styles/RoomStyle/RoomHero.styled.js
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const HeroCon = styled.section``;
