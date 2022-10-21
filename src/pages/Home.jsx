80ee5bc01cf02dbef67e2b37789d6e28ca620c3e
gift56 <efeasiughu@gmail.com>
2022-10-21 14:24:36 +0100

    done fixing bug and errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import AboutUs from '../components/Home/AboutUs'
+import AboutUs from "../components/Home/AboutUs";
 import BestApartment from "../components/Home/BestApartment";
 import Facilities from "../components/Home/Facilities";
 import Gallery from "../components/Home/Gallery";
 import GetInTourch from "../components/Home/GetInTourch";
 import Hero from "../components/Home/Hero";
+import RoomandApartment from "../components/Home/RoomandApartment";
 import Layout from "../components/Layout/Layout";
-import RoomandApartment from "../components/RoomandApartment";
 
 const Home = () => {
   return (
