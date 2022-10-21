6d19de68292f8b9a136a25627e5adf315dc9aaa1
gift56 <efeasiughu@gmail.com>
2022-10-21 15:55:57 +0100

    writing jsx

--- src/components/RoomsData/RoomDetail.jsx
+++ src/components/RoomsData/RoomDetail.jsx
@@ -15,8 +15,28 @@ const RoomDetail = () => {
     <DetailCon>
       <Container>
         <DetailArea>
-          <DetailLeftArea></DetailLeftArea>
-          <DetailRightArea></DetailRightArea>
+          <DetailLeftArea>
+            <div className="leftSide">
+              <div className="detail">
+                <Button color="#FFFCF6" />
+                <h3>
+                  All suites have a unique design because we want our every
+                  guest to feel special.
+                </h3>
+              </div>
+              <h4>Premier Standard</h4>
+            </div>
+          </DetailLeftArea>
+          <DetailRightArea>
+            <p>
+              The Superior twin would perfectly match the needs of those who
+              plan to stay long. The bright and airy room is equipped with
+              superior soft Italian furniture. Large windows open a beautiful
+              view to the historical part of the city. Contemporary interior
+              design and comfortable beds will make your stay cozy and
+              delightful.
+            </p>
+          </DetailRightArea>
         </DetailArea>
       </Container>
     </DetailCon>
