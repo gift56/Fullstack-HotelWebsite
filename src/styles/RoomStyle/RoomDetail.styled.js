c460dd475e4182d364729d3a147a309db62a358d
gift56 <efeasiughu@gmail.com>
2022-10-21 15:39:12 +0100

    added detail image

---
+++ src/styles/RoomStyle/RoomDetail.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const DetailCon = styled.section``;
+
+export const DetailArea = styled.div``;
+
+export const DetailLeftArea = styled.div``;
+
+export const DetailRightArea = styled.div``;
