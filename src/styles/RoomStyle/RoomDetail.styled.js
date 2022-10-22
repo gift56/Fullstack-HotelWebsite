0806334bb30d6bb3e78004ba65f1185c65fcf0a1
gift56 <efeasiughu@gmail.com>
2022-10-22 09:25:48 +0100

    fixed button size

--- src/styles/RoomStyle/RoomDetail.styled.js
+++ src/styles/RoomStyle/RoomDetail.styled.js
@@ -25,6 +25,34 @@ export const DetailArea = styled.div`
       display: flex;
       flex-direction: column-reverse;
       width: 100%;
+      align-items: flex-start;
+      justify-content: flex-start;
+      gap: 2rem;
+      button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: transparent;
+        border: 1px solid #fffcf6;
+        border-radius: 99px;
+        width: 175px;
+        height: 50px;
+        div {
+          width: 35px;
+          height: 35px;
+          img {
+            width: 100%;
+          }
+        }
+        span {
+          font-weight: 400;
+          font-family: "Open Sans", sans-serif !important;
+          font-size: 16px;
+          line-height: 19px;
+          text-transform: capitalize;
+          color: #fffcf6;
+        }
+      }
     }
   }
 `;
