f839cee45db2a90bfdcde329602e629e11bf6754
gift56 <efeasiughu@gmail.com>
2022-10-22 11:16:51 +0100

    imported styled

--- src/components/RoomsData/RoomOptions.jsx
+++ src/components/RoomsData/RoomOptions.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Container } from "../../styles/Container/container.styled";
+import {
+  OptionArea,
+  OptionCon,
+  OptionLeftArea,
+  OptionRightArea,
+} from "../../styles/RoomStyle/RoomOptions.styled";
 
 const RoomOptions = () => {
   return (
-    <div>RoomOptions</div>
-  )
-}
+    <OptionCon>
+      <Container>
+        <OptionArea>
+          <OptionLeftArea></OptionLeftArea>
+          <OptionRightArea></OptionRightArea>
+        </OptionArea>
+      </Container>
+    </OptionCon>
+  );
+};
 
-export default RoomOptions
+export default RoomOptions;
