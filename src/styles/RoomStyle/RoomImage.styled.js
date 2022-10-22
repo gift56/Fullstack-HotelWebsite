33df85c4431f3c3f0e84b41af49a73f47110e3be
gift56 <efeasiughu@gmail.com>
2022-10-22 10:44:57 +0100

    added bg

--- src/styles/RoomStyle/RoomImage.styled.js
+++ src/styles/RoomStyle/RoomImage.styled.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const ImageCon = styled.section`
   width: 100%;
   background-color: ${({ theme }) => theme.darkColor};
+  height: 427px;
+  background-image: url(${({ bg }) => bg});
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const ImageArea = styled.section``;
