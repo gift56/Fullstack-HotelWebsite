89e11d6e1afcfb117f45dca3dbbe75950d904c50
gift56 <efeasiughu@gmail.com>
2022-10-22 11:19:01 +0100

    imported images

--- src/components/RoomsData/RoomOptions.jsx
+++ src/components/RoomsData/RoomOptions.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import MainImg from '../../assets/roomMainImg.png';
+import LeftImg from '../../assets/leftImg.png';
 import { Container } from "../../styles/Container/container.styled";
 import {
   OptionArea,
