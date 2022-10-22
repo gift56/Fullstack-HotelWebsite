483edcb0623219c668b03c471a4c3de3f07cddb1
gift56 <efeasiughu@gmail.com>
2022-10-22 10:41:47 +0100

    passed props

--- src/components/RoomsData/DetailImage.jsx
+++ src/components/RoomsData/DetailImage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DetailImg from "../../assets/detailBg.png";
 import { Container } from "../../styles/Container/container.styled";
-import { ImageCon } from "../../styles/RoomStyle/RoomImage.styled";
+import { ImageArea, ImageCon } from "../../styles/RoomStyle/RoomImage.styled";
 
 const DetailImage = () => {
   return (
     <ImageCon bg={DetailImg}>
-      <Container></Container>
+      <Container>
+        <ImageArea></ImageArea>
+      </Container>
     </ImageCon>
   );
 };
