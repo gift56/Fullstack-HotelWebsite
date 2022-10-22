12cbed368f01cd1da7c94007637663a56c60f050
gift56 <efeasiughu@gmail.com>
2022-10-22 11:49:31 +0100

    adding stylings

--- src/styles/RoomStyle/RoomOptions.styled.js
+++ src/styles/RoomStyle/RoomOptions.styled.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
-export const OptionCon = styled.section``;
+export const OptionCon = styled.section`
+  background-color: ${({ theme }) => theme.whiteColor};
+`;
 
-export const OptionArea = styled.div``;
+export const OptionArea = styled.div`
+  padding: 40px 0;
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 2rem;
+`;
 
 export const OptionLeftArea = styled.div``;
 
