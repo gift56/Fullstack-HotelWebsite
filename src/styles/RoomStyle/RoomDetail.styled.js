a49ec1dc132c9fb7f993dc50b12d6374c8a2ed78
gift56 <efeasiughu@gmail.com>
2022-10-22 09:06:55 +0100

    added some styings

--- src/styles/RoomStyle/RoomDetail.styled.js
+++ src/styles/RoomStyle/RoomDetail.styled.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const DetailCon = styled.section``;
+export const DetailCon = styled.section`
+  position: relative;
+  background-color: ${({ theme }) => theme.darkColor};
+`;
 
 export const DetailArea = styled.div``;
 
