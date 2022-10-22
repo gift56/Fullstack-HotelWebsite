cb1b062158e03480244a2867380c5403a5aa10f8
gift56 <efeasiughu@gmail.com>
2022-10-22 09:07:40 +0100

    added z-index

--- src/styles/RoomStyle/RoomDetail.styled.js
+++ src/styles/RoomStyle/RoomDetail.styled.js
@@ -3,9 +3,12 @@ import styled from "styled-components";
 export const DetailCon = styled.section`
   position: relative;
   background-color: ${({ theme }) => theme.darkColor};
+  z-index: 1;
 `;
 
-export const DetailArea = styled.div``;
+export const DetailArea = styled.div`
+  padding: 60px 0;
+`;
 
 export const DetailLeftArea = styled.div``;
 
