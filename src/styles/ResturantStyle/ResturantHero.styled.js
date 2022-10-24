1a9c4899664d8318419b614cc4e02f4d6df4e623
gift56 <efeasiughu@gmail.com>
2022-10-24 10:46:15 +0100

    done with styings

--- src/styles/ResturantStyle/ResturantHero.styled.js
+++ src/styles/ResturantStyle/ResturantHero.styled.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
 export const ResturantCon = styled.section``;
+
+export const ResturantArea = styled.div``;
+
+export const ResturantLeftArea = styled.div``;
+
+export const ResturantRightArea = styled.div``;
