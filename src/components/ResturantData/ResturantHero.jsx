1b578f45f5dac734206f327226002d8ea5e657ec
gift56 <efeasiughu@gmail.com>
2022-10-24 11:04:12 +0100

    added container class

--- src/components/ResturantData/ResturantHero.jsx
+++ src/components/ResturantData/ResturantHero.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Label from "../../assets/Alabel.svg";
 import BgLine from "../../assets/Bglines.svg";
 import { ResturantCon } from "../../styles/ResturantStyle/ResturantHero.styled";
+import { Container } from "../../styles/Container/container.styled";
 
 const ResturantHero = () => {
-  return <ResturantCon>
-    
-  </ResturantCon>;
+  return (
+    <ResturantCon>
+      <Container></Container>
+    </ResturantCon>
+  );
 };
 
 export default ResturantHero;
