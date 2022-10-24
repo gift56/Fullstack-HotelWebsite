83c7b206342c09f71d31fccd5a5c1f7b728a92cb
gift56 <efeasiughu@gmail.com>
2022-10-24 11:27:39 +0100

    adding border bottom to header

--- src/styles/ResturantStyle/ResturantHero.styled.js
+++ src/styles/ResturantStyle/ResturantHero.styled.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const ResturantCon = styled.section`
   background-color: ${({ theme }) => theme.darkColor};
   background-image: url(${({ bg }) => bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 `;
 
 export const ResturantArea = styled.div``;
