9a3a0b0ec45641204bdee15598a5c42754cb2217
gift56 <efeasiughu@gmail.com>
2022-10-24 14:34:49 +0100

    updating some stuff

--- src/styles/ResturantStyle/ResturantSpace.styled.js
+++ src/styles/ResturantStyle/ResturantSpace.styled.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
-export const ResturantSpaceCon = styled.section``;
+export const ResturantSpaceCon = styled.section`
+  background-color: ${({ theme }) => theme.darkColor};
+`;
 
-export const ResturantSpaceArea = styled.div``;
+export const ResturantSpaceArea = styled.div`
+  padding: 40px 0;
+`;
 
 export const ResturantSpaceLeftArea = styled.div``;
 
