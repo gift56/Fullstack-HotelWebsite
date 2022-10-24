d45d8ffb68ef5da2663c965bb15aeb9b7022c49a
gift56 <efeasiughu@gmail.com>
2022-10-24 10:40:18 +0100

    added new stuff

--- src/pages/Resturants.jsx
+++ src/pages/Resturants.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-
+import ResturantHero from "../components/ResturantData/ResturantHero";
 
 const Resturants = () => {
-  return <Layout>
-    
-  </Layout>;
+  return (
+    <Layout>
+      <ResturantHero />
+    </Layout>
+  );
 };
 
 export default Resturants;
