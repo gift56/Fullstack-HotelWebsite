c4fe6d79186d45352efa2a1bcdac2257d3ab6074
gift56 <efeasiughu@gmail.com>
2022-10-24 14:38:29 +0100

    working on image width

--- src/styles/ResturantStyle/ResturantSpace.styled.js
+++ src/styles/ResturantStyle/ResturantSpace.styled.js
@@ -16,4 +16,20 @@ export const ResturantSpaceArea = styled.div`
 
 export const ResturantSpaceLeftArea = styled.div``;
 
-export const ResturantSpaceRightArea = styled.div``;
+export const ResturantSpaceRightArea = styled.div`
+  width: inherit;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  .imageCon {
+    width: 100%;
+    .image {
+      width: 572px;
+      img {
+        width: 100%;
+      }
+    }
+  }
+`;
