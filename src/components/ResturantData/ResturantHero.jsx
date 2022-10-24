7ebbdfe4a36dc2363c44c9ad7fd79a57ddbeee89
gift56 <efeasiughu@gmail.com>
2022-10-24 10:48:38 +0100

    done importing images

--- src/components/ResturantData/ResturantHero.jsx
+++ src/components/ResturantData/ResturantHero.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import Label from "../../assets/Alabel.svg";
+import BgLine from "../../assets/Bglines.svg";
 
 const ResturantHero = () => {
-  return (
-    <div>ResturantHero</div>
-  )
-}
+  return <div>ResturantHero</div>;
+};
 
-export default ResturantHero
+export default ResturantHero;
