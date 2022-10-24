6978123984b7fa1c2e75d557ceafa98a2f2d06bf
gift56 <efeasiughu@gmail.com>
2022-10-24 14:22:36 +0100

    added left and right area

--- src/components/ResturantData/ResturantSpace.jsx
+++ src/components/ResturantData/ResturantSpace.jsx
@@ -4,6 +4,8 @@ import RightImg from "../../assets/resturantspace1.png";
 import {
   ResturantSpaceArea,
   ResturantSpaceCon,
+  ResturantSpaceLeftArea,
+  ResturantSpaceRightArea,
 } from "../../styles/ResturantStyle/ResturantSpace.styled";
 import { Container } from "../../styles/Container/container.styled";
 
@@ -12,7 +14,12 @@ const ResturantSpace = () => {
     <ResturantSpaceCon>
       <Container>
         <ResturantSpaceArea>
-            
+          <ResturantSpaceLeftArea></ResturantSpaceLeftArea>
+          <ResturantSpaceRightArea>
+            <div className="titleArea">
+              <h3></h3>
+            </div>
+          </ResturantSpaceRightArea>
         </ResturantSpaceArea>
       </Container>
     </ResturantSpaceCon>
