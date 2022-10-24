245289e7091e37a1b48cc7ddc7f2f91e1b06e0e0
gift56 <efeasiughu@gmail.com>
2022-10-24 13:01:41 +0100

    added some new stuff

--- src/components/ResturantData/ResturantOffer.jsx
+++ src/components/ResturantData/ResturantOffer.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import MobileImg from '../../assets/resturant1.png';
-import DesktopImg from '../../assets/resturant2.png';
+import React from "react";
+import MobileImg from "../../assets/resturant1.png";
+import DesktopImg from "../../assets/resturant2.png";
+import { Container } from "../../styles/Container/container.styled";
+import {
+  ResturantOfferArea,
+  ResturantOfferCon,
+  ResturantOfferLeftArea,
+  ResturantOfferRightArea,
+} from "../../styles/ResturantStyle/ResturantOffer.styled";
 
 const ResturantOffer = () => {
   return (
-    <div>ResturantOffer</div>
-  )
-}
+    <ResturantOfferCon>
+      <Container>
+        <ResturantOfferArea>
+          <ResturantOfferLeftArea></ResturantOfferLeftArea>
+          <ResturantOfferRightArea></ResturantOfferRightArea>
+        </ResturantOfferArea>
+      </Container>
+    </ResturantOfferCon>
+  );
+};
 
-export default ResturantOffer
+export default ResturantOffer;
--- src/styles/ResturantStyle/ResturantOffer.styled.js
+++ src/styles/ResturantStyle/ResturantOffer.styled.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
 export const ResturantOfferCon = styled.section``;
+
+export const ResturantOfferArea = styled.div``;
+
+export const ResturantOfferLeftArea = styled.div``;
+
+export const ResturantOfferRightArea = styled.div``;
