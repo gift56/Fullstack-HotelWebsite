0d150d166eac9f43d243a39ab1474a015b0c9b75
gift56 <efeasiughu@gmail.com>
2022-10-24 12:40:34 +0100

    removed space

--- src/components/ResturantData/ResturantHero.jsx
+++ src/components/ResturantData/ResturantHero.jsx
@@ -29,11 +29,11 @@ const ResturantHero = () => {
                 <span>Bank Hotel proudly welcomes you to the</span>{" "}
                 <span>Safe Restaurant, a place where you will</span>
                 {` `}
-                <span>wish you could dine every day. The</span>{" "}
-                <span>restaurant offers a unique menu developed</span>{" "}
-                <span>by the team of professionals led by chef</span>{" "}
-                <span>Mary-Ann Jones, the wine list with more</span>{" "}
-                <span>than 250 items, the outstanding service,</span>{" "}
+                <span>wish you could dine every day. The</span>
+                <span>restaurant offers a unique menu developed</span>
+                <span>by the team of professionals led by chef</span>
+                <span>Mary-Ann Jones, the wine list with more</span>
+                <span>than 250 items, the outstanding service,</span>
                 <span>and the unforgettable atmosphere.</span>
               </p>
             </div>
