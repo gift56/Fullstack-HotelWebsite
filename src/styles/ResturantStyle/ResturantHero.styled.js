5594194d2f6505d7c2f1d1ca42ae2bcf6681fd7f
gift56 <efeasiughu@gmail.com>
2022-10-24 11:22:12 +0100

    stying some stuff

--- src/styles/ResturantStyle/ResturantHero.styled.js
+++ src/styles/ResturantStyle/ResturantHero.styled.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const ResturantCon = styled.section``;
+export const ResturantCon = styled.section`
+  background-color: ${({ theme }) => theme.darkColor};
+  background-image: url(${({ bg }) => bg});
+`;
 
 export const ResturantArea = styled.div``;
 
