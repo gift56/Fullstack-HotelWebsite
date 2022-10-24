200fa91726b3e8d905f540daa2a7d6b6d3a815ae
gift56 <efeasiughu@gmail.com>
2022-10-24 14:20:26 +0100

    working on resturant container area

--- src/components/ResturantData/ResturantSpace.jsx
+++ src/components/ResturantData/ResturantSpace.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import LeftImg from '../../assets/resturantspace2.png';
-import RightImg from '../../assets/resturantspace1.png';
+import React from "react";
+import LeftImg from "../../assets/resturantspace2.png";
+import RightImg from "../../assets/resturantspace1.png";
+import { ResturantSpaceCon } from "../../styles/ResturantStyle/ResturantSpace.styled";
+import { Container } from "../../styles/Container/container.styled";
 
 const ResturantSpace = () => {
   return (
-    <div>ResturantSpace</div>
-  )
-}
+    <ResturantSpaceCon>
+      <Container></Container>
+    </ResturantSpaceCon>
+  );
+};
 
-export default ResturantSpace
+export default ResturantSpace;
