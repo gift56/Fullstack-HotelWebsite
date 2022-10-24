1ad753ce57bce4ae0c37870910e195fab6a1d470
gift56 <efeasiughu@gmail.com>
2022-10-24 11:15:45 +0100

    added span tag

--- src/components/ResturantData/ResturantHero.jsx
+++ src/components/ResturantData/ResturantHero.jsx
@@ -14,8 +14,21 @@ const ResturantHero = () => {
     <ResturantCon bg={BgLine}>
       <Container>
         <ResturantArea>
-          <ResturantLeftArea></ResturantLeftArea>
-          <ResturantRightArea></ResturantRightArea>
+          <ResturantLeftArea>
+            <h2>“Safe” restaurant</h2>
+          </ResturantLeftArea>
+          <ResturantRightArea>
+            <h3>One of a Kind</h3>
+            <div className="details">
+              <p>
+                <span>Bank Hotel proudly welcomes you to the</span> Safe Restaurant, a place
+                where you will wish you could dine every day. The restaurant
+                offers a unique menu developed by the team of professionals led
+                by chef Mary-Ann Jones, the wine list with more than 250 items,
+                the outstanding service, and the unforgettable atmosphere.
+              </p>
+            </div>
+          </ResturantRightArea>
         </ResturantArea>
       </Container>
     </ResturantCon>
