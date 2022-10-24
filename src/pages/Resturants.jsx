5c61cbe1c3822904dc93a5738adbb31ff7c58db1
gift56 <efeasiughu@gmail.com>
2022-10-24 14:16:45 +0100

    imported resturantspace components

--- src/pages/Resturants.jsx
+++ src/pages/Resturants.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 import ResturantHero from "../components/ResturantData/ResturantHero";
 import ResturantOffer from "../components/ResturantData/ResturantOffer";
+import ResturantSpace from "../components/ResturantData/ResturantSpace";
 
 const Resturants = () => {
   return (
     <Layout>
       <ResturantHero />
       <ResturantOffer />
+      <ResturantSpace />
     </Layout>
   );
 };
