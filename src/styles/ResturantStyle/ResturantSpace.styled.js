8f76fb634da370763a8246e611f16fb17c8f65bf
gift56 <efeasiughu@gmail.com>
2022-10-24 14:19:13 +0100

    added space left and right area

--- src/styles/ResturantStyle/ResturantSpace.styled.js
+++ src/styles/ResturantStyle/ResturantSpace.styled.js
@@ -3,3 +3,7 @@ import styled from "styled-components";
 export const ResturantSpaceCon = styled.section``;
 
 export const ResturantSpaceArea = styled.div``;
+
+export const ResturantSpaceLeftArea = styled.div``;
+
+export const ResturantSpaceRightArea = styled.div``;
