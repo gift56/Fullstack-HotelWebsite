24681728fdde0314c406dc18e533d218ccbc52cc
gift56 <efeasiughu@gmail.com>
2023-01-04 07:00:54 -0800

    done working on 404 page

--- src/routes/RoutedPages.jsx
+++ src/routes/RoutedPages.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import About from "../pages/About";
 import Home from "../pages/Home";
 import Page404 from "../pages/Page404";
 import Resturants from "../pages/Resturants";
@@ -16,7 +15,6 @@ const RoutedPages = () => {
       <Routes>
         <Route path="/" element={<Home getBg={getBg} />} />
         <Route path="/rooms" element={<Room getBg={getBg} />} />
-        <Route path="/about" element={<About />} />
         <Route path="/restaurant" element={<Resturants />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
