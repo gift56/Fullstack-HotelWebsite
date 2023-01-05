66566fd9000c60a55253dbc76db3f770a4b0a01f
gift56 <efeasiughu@gmail.com>
2023-01-05 12:47:32 -0800

    added some stuff

--- src/components/Home/Facilities.jsx
+++ src/components/Home/Facilities.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FacilityImg from "../../assets/facilityH.svg";
-import FacilitySmallImg from "../../assets/facilitySmI.svg";
+import FacilitySmallImg from "../../assets/facilitySmI.png";
 import FacilityStar from "../../assets/facilityStar.svg";
 import FacilityLabel from "../../assets/facilityLabel.svg";
 import FacilityDesImg from "../../assets/facilityDesImg.svg";
